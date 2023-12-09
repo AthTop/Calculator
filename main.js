cc58baff99a28d4aa0a2ce6643380ee3ce26bb02
Sakis T. <106918030+AthTop@users.noreply.github.com>
2023-12-09 18:24:58 +0200

    Complete basic functionallity. Calculations possible with operator buttons instead of only equals

--- main.js
+++ main.js
@@ -15,16 +15,16 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(a, b, operator) {
+function operate() {
     switch(operator) {
         case '+' :
-            return add(a, b);
+            return add(operands[operands.length-2], operands[operands.length-1]);
         case '-' :
-            return subtract(a, b);
+            return subtract(operands[operands.length-2], operands[operands.length-1]);
         case '*' :
-            return multiply(a, b);
+            return multiply(operands[operands.length-2], operands[operands.length-1]);
         case '/' :
-            return divide(a, b);
+            return divide(operands[operands.length-2], operands[operands.length-1]);
     }
 }
 
@@ -33,20 +33,36 @@ function isAlphanumeric(str) {
 }
 
 function parseNumber(e) {
-    if(isAlphanumeric(e.target.value)) {
-        displayNumber += e.target.value;
-    }
-    console.log(displayNumber);
+    if(isAlphanumeric(e.target.value)) currentNumber += e.target.value;
 }
 
-let displayNumber = "";
+function parseOperator(e) {
+    operator = e.target.value;
+    operands.push(Number(currentNumber));
+    if (operands.length > 1) {
+        operands.push(operate());
+        console.log(operands[operands.length-1]);
+    }
+    currentNumber = "";
+}
+
+
+
+let currentNumber = "";
+let operands = [];
+let operator;
+
 const numbers = document.querySelectorAll('.numbers');
 numbers.forEach(number => number.addEventListener('click', parseNumber))
+const operators = document.querySelectorAll('.operators');
+operators.forEach(e => e.addEventListener('click', parseOperator))
+const equal = document.querySelector('.equal');
+equal.addEventListener('click', () => {
+    operands.push(Number(currentNumber));
+    result = operate();
+    console.log(result);
+    operands.splice(0, operands.length);
+});
 
 
 
-let firstNumber = 2;
-let secondNumber = 3;
-let operator = '-';
-
-console.log(operate(firstNumber, secondNumber, operator));
