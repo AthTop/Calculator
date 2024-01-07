0ea8c295854be463e94fbd03bbc1f483eb22b359
Sakis T. <106918030+AthTop@users.noreply.github.com>
2024-01-07 15:06:45 +0200

    Fix bugs, can chain operations properly now

--- main.js
+++ main.js
@@ -29,48 +29,40 @@ function operate(a, b) {
     }
 }
 
-function createNumberDisplayElement(text) {
-    const para = document.createElement('p');
-    const node = document.createTextNode(text);
-    para.appendChild(node);
-    return para;
-}
-
-function updateCurrentNumberDisplay(number) {
-    currentNumberDisplay.textContent = number;
-}
-
-function updateDisplayDiv(value) {
-    displayDiv.insertBefore(createNumberDisplayElement(value), currentNumberDisplay);
-}
 
 const displayDiv = document.querySelector('#display');
 const currentNumberDisplay = document.querySelector('#currentNumber');
 let currentNumber = "";
 let firstOperand = "";
-let secondOperand;
-let operator;
+let secondOperand = "";
+let operator = "";
 let result;
 
 const numbers = document.querySelectorAll('.numbers');
 numbers.forEach(number => number.addEventListener('click', (e) => {
     currentNumber += e.target.value;
-    updateCurrentNumberDisplay(currentNumber);
+    if (firstOperand !== "") {
+        displayDiv.textContent = firstOperand + operator + currentNumber;
+    }
+    else {
+        displayDiv.textContent = currentNumber;
+    }
 }));
 
 // Listen for operator push and attach the current number and operator to display
 // and prepare for the next number
 const operators = document.querySelectorAll('.operators');
 operators.forEach(e => e.addEventListener('click', (e) => {
-    operator = e.target.value;
-    if (firstOperand !== "") equalOperate(operator)
+    if (operator !== ""){
+        equalOperate(operator)
+    }
+    if (firstOperand !== "") {}
     else {
         firstOperand = currentNumber;
         currentNumber = "";
-        updateDisplayDiv(firstOperand);
-        updateDisplayDiv(operator);
-        updateCurrentNumberDisplay(currentNumber);
-    }    
+        displayDiv.textContent = firstOperand + operator;
+    }
+    operator = e.target.value;    
 }));
 
 // Listen for equal push, perform operation between the 2 operands
@@ -82,15 +74,28 @@ equal.addEventListener('click', () =>{
 
 function equalOperate(operator) {
     secondOperand = currentNumber;
-    updateDisplayDiv(secondOperand);
-    updateDisplayDiv(operator);
-    currentNumber = operate(+firstOperand, +secondOperand);
-    updateCurrentNumberDisplay(currentNumber);
-    firstOperand = secondOperand;
+    result = operate(+firstOperand, +secondOperand).toFixed(1);
+    if (operator === "=") {
+        displayDiv.textContent += operator + result;
+        firstOperand = result;
+    }
+    else {
+        firstOperand = result;
+        displayDiv.textContent = firstOperand + operator;
+    }
     secondOperand = "";
     currentNumber = "";
+    operator = "";
 }
 
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', () => {
+    displayDiv.textContent = " ";
+    firstOperand = "";
+    secondOperand = "";
+    operator = "";
+    currentNumber = "";
+});
 
 
 
